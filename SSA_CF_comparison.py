1a9ab8f1f9065a680855464c1274f5b9410f2e40
ChengDaHaI <chengdahai615@gmail.com>
2017-05-05 19:11:16 +0800

    comparison to SSA

---
+++ SSA_CF_comparison.py
@@ -0,0 +1,90 @@
+'''
+This file aims to generate simulation result of CCF in MIMO C-RAN system.
+We will compare CCF with SSA PNC scheme.
+Date :2017-5-4
+'''
+
+from sage.all import *
+from NEW_basic import *
+from CoF_LLL import Find_A_and_Rate
+from scipy import optimize
+from math import log, log10, fabs
+import time
+import copy
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Compute the sum rate of CCF in a MIMO C-RAN system
+# A user with N antenna is viewed as N independent users
+# input: P_tran -- transmiter power list, H_a -- first hop channel,
+#        capacity_bh -- capcaity of backhual, N -- number of antenna in transmiter
+# output:sum rate of a C-RAN system
+def Rate_CCF_CRAN( P_tran, H_a, capacity_bh):
+    # transmitter power
+    P_vec = vector(RR, P_tran)
+    P_mat = matrix.diagonal([sqrt(x) for x in P_vec])
+    # Use LLL to find a good A matrix
+    # determine the fine lattice of m-th relay at the same time
+    try:
+        (A_best_LLL, source_rate_upbound_list, relay_fine_lattices) = Find_A_and_Rate(P_mat, P_vec, H_a, True, vector(RR, [1]*L))
+    except:
+        print 'error in seeking A and rate'
+        raise
+    # assuming SW coding is used and thus the forwarding rate is the sum of source message rates
+    return max(min(sum(source_rate_upbound_list), capacity_bh), 0)
+
+
+# optimized sum rate of a C-RAN system
+# input: P_con -- power budget of transmiter, C_BH -- coefficient of capacity_bh, alg -- used algorithm
+# output:optimized sum rate
+@parallel(ncpus=Cores)
+def rate_opt(P_con,  C_BH,  alg = 'None'):
+
+    set_random_seed()
+    # generate channel matrix with M*N rows and L*N columns
+    H = Matrix(RR, M, L,  lambda i, j: normalvariate(0, 1))
+    capacity_bh = C_BH * log(P_con)
+    sum_rate = 0
+    if alg == 'DE':
+        opt_fun = lambda x: - Rate_CCF_CRAN(x, H, capacity_bh)
+        bounds = ((0.1, P_con), ) * L
+        opt_res = optimize.differential_evolution(opt_fun, bounds, maxiter = 20, disp = False)
+        sum_rate = -opt_res.fun
+        print 'CCF Differential Evolution:', opt_res.success
+    elif alg == 'None':
+        sum_rate = Rate_CCF_CRAN([P_con]*L, H, capacity_bh)
+        print 'CCF None Optimization!'
+
+    return sum_rate
+
+if __name__ == '__main__':
+
+    C_BH = 5
+    alg = 'DE'
+    num_batch = 500
+    sum_rate_list = []
+    New_sum_rate = []
+    # PI_con = [10 ** 1, 10 ** 1.25, 10 ** 1.5, 10 ** 1.75, 10 ** 2, 10 ** 2.25, 10 ** 2.5, 10 ** 2.75, 10 ** 3, 10 ** 3.25,
+    #        10 ** 3.5]
+    PI_con = [10 ** 2]
+    print 'Simulation Starts!\n'
+    t1 = time.time()
+    for Pi in PI_con:
+        print 'Transmitter Power:', Pi
+        # the power of each antenna user is Pi/N
+        # test_rate = rate_opt(Pi/N, C_BH, alg)
+        # print 'Test'
+        # Rate_list = []
+        # for i in range(num_batch):
+        #     Rate_list.append(rate_opt(Pi, C_BH, alg))
+        result_list = list(rate_opt([(Pi, C_BH,  alg )]* num_batch))
+        Rate_list = [result_list[i][1] for i in range(0, num_batch)]
+        sum_rate = sum(Rate_list)/num_batch
+        sum_rate_list.append(sum_rate)
+    t2 = time.time()
+    print 'Total Time Cost: ', (t2 - t1)
+    PI_dB = [10 * log10(P_con) for P_con in PI_con]
+    Full_Result = np.column_stack((PI_dB, sum_rate_list))
+    np.savetxt('/home/haizi/PycharmProjects/SSA/Simu_result/' + 'CCF_OPT' +' iter =' + num_batch.__str__() + ' K=' + K.__str__() + ' N=' + N.__str__() + time.ctime()
+               + 'Simu_Data.txt', Full_Result, fmt='%1.5e')
